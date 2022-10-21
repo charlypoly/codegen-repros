f7c2e036da2215944cba4ebf89ce38b77a0a3a7f
Charly POLY <cpoly55@gmail.com>
2022-10-21 10:21:54 +0200

    client-preset "fragments loops"

---
+++ client-preset-fragments-loops/src/App.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+
+import "./App.css";
+import Film from "./Film";
+import { graphql } from "./gql/gql";
+
+const allFilmsWithVariablesQueryDocument = graphql(/* GraphQL */ `
+  query allFilmsWithVariablesQuery($first: Int!) {
+    allFilms(first: $first) {
+      edges {
+        node {
+          id
+          ...FilmItem
+        }
+      }
+    }
+  }
+`);
+
+function App() {
+  const { data } = useQuery(allFilmsWithVariablesQueryDocument, {
+    variables: { first: 10 },
+  });
+  return (
+    <div className="App">
+      {data && (
+        <ul>
+          {data.allFilms?.edges?.map(
+            (e, i) => e?.node && <Film film={e?.node} key={`film-${e.node}`} />
+          )}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default App;
